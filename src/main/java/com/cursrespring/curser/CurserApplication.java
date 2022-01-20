a1131e84357f20853a2db1284f3325781b17f171
Ruanxbrx <ruan10d5@hotmail.com>
2022-01-20 12:01:07 -0300

    creado

---
+++ src/main/java/com/cursrespring/curser/CurserApplication.java
@@ -0,0 +1,13 @@
+package com.cursrespring.curser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursrespring/curser/CurserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursrespring.curser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
