4420c57271cc00592a507c1ecc5a612d7e9d0109
Ruanxbrx <ruan10d5@hotmail.com>
2022-01-21 16:53:30 -0300

    Subtotal & Total methods

--- src/main/java/com/cursrespring/curser/entities/Payment.java
+++ src/main/java/com/cursrespring/curser/entities/Payment.java
@@ -1,5 +1,7 @@
 package com.cursrespring.curser.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -13,6 +15,8 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+
+    @JsonIgnore
     @ManyToOne
     @MapsId
     private Order order;
