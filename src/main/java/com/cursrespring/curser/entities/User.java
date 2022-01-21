4282d032d17f9dd0cab3fc0d11a36fe8e169e592
Ruanxbrx <ruan10d5@hotmail.com>
2022-01-21 10:48:22 -0300

    Order, Instant, ISO 8601 parte 1

--- src/main/java/com/cursrespring/curser/entities/User.java
+++ src/main/java/com/cursrespring/curser/entities/User.java
@@ -1,13 +1,13 @@
 package com.cursrespring.curser.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID =1L;
 
@@ -19,6 +19,9 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
     }
 
@@ -28,6 +31,11 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
