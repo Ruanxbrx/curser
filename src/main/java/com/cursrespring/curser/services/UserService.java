c485c1bf78e80224e8faa3c43eb8f7f9bbdc3ff9
Ruanxbrx <ruan10d5@hotmail.com>
2022-01-24 11:16:13 -0300

    Exception handling - findById

--- src/main/java/com/cursrespring/curser/services/UserService.java
+++ src/main/java/com/cursrespring/curser/services/UserService.java
@@ -2,6 +2,7 @@ package com.cursrespring.curser.services;
 
 import com.cursrespring.curser.entities.User;
 import com.cursrespring.curser.repositories.UserRepository;
+import com.cursrespring.curser.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
