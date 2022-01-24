c60a465044fa50f4d4bf33a1af689e5bb35ddef8
Ruanxbrx <ruan10d5@hotmail.com>
2022-01-24 10:00:14 -0300

    User delete

--- src/main/java/com/cursrespring/curser/controllers/UserController.java
+++ src/main/java/com/cursrespring/curser/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.cursrespring.curser.entities.User;
 import com.cursrespring.curser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -35,5 +36,11 @@ public class UserController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
