9dc216eff3c934b23a23ccaaa6d912978fac6ffb
Ruanxbrx <ruan10d5@hotmail.com>
2022-02-03 16:21:45 -0300

    Refactorando classe OderService para OrderServiceTeste

--- src/test/java/com/cursrespring/curser/services/OrderServiceTeste.java
+++ src/test/java/com/cursrespring/curser/services/OrderServiceTeste.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class OrderService {
+public class OrderServiceTeste {
 
     @Autowired
     private OrderRepository repository;
