3740cb09e9284405eeec1d15f286ef7555c7e2d9
Ruanxbrx <ruan10d5@hotmail.com>
2022-02-07 15:52:20 -0300

    Teste unitario ResourceExceptionHandlerTest criado

--- src/test/java/com/cursrespring/curser/controllers/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/com/cursrespring/curser/controllers/exceptions/ResourceExceptionHandlerTest.java
@@ -2,30 +2,55 @@ package com.cursrespring.curser.controllers.exceptions;
 
 import com.cursrespring.curser.services.exceptions.DatabaseExepetion;
 import com.cursrespring.curser.services.exceptions.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.servlet.http.HttpServletRequest;
-import java.time.Instant;
+import static org.junit.Assert.assertNotNull;
 
-@ControllerAdvice
+@ExtendWith(SpringExtension.class)
 public class ResourceExceptionHandlerTest {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardErrorTest> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
-        String error = "Resource not found";
-        HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardErrorTest err = new StandardErrorTest(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
-        return ResponseEntity.status(status).body(err);
+    @Mock
+    private DatabaseExepetion databaseExepetion;
+
+    @Mock
+    private ResourceNotFoundException resourceNotFoundException;
+
+    @InjectMocks
+    private ResourceExceptionHandler resourceExceptionHandler;
+
+    @Test
+    @DisplayName("Deve tratar  Exeption recurso nao encontrado")
+    void deveTradaExeptionResourceNotFound(){
+        //cenario
+        ResourceNotFoundException e = new ResourceNotFoundException(1L);
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        //execução
+        ResponseEntity<StandardError> obj =resourceExceptionHandler.resourceNotFound(e,request);
+
+        //verificação
+        assertNotNull(obj);
     }
 
-    @ExceptionHandler(DatabaseExepetion.class)
-    public ResponseEntity<StandardErrorTest> database(DatabaseExepetion e, HttpServletRequest request){
-        String error = "Database error";
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        StandardErrorTest err = new StandardErrorTest(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
-        return ResponseEntity.status(status).body(err);
+    @Test
+    @DisplayName("Deve tratar  Exeption de Integridade de banco de dados")
+    void deveTradaExeptionDeIntegridaDeBanco(){
+        //cenario
+        DatabaseExepetion e = new DatabaseExepetion("test");
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        //execução
+        ResponseEntity<StandardError> obj =resourceExceptionHandler.database(e,request);
+
+        //verificação
+        assertNotNull(obj);
     }
+
 }
